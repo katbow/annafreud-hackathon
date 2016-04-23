7aeb9fad4cc5ae0315e72d8d0a4504268b0bc57a
Andrew <a.macmurray@icloud.com>
2016-04-23 18:58:32 +0100

    add collapsible panel

--- frontend/src/js/components/Client/client_index.js
+++ frontend/src/js/components/Client/client_index.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Row, Col, Panel } from 'react-bootstrap'
-import StatusIcon from '../StatusIcon/statusIcon_index.js'
+import { Panel, Row, Col } from 'react-bootstrap'
+import ClientItem from './clientItem.js'
 
-const Client = (props) => {
-  return (
-    <Col className='client-item' xs={10} xsOffset={1}>
-      <Row>
-        <Col className='client-name' xs={2}>
-          <p>{props.firstName}</p>
-        </Col>
-        <Col className='open-letters' xs={3} xsOffset={4}>
-          <p>{props.numberOfLetters} open letters</p>
-        </Col>
-        <Col className='status-icon' xs={1}>
-          <StatusIcon status='red'/>
-        </Col>
-      </Row>
-    </Col>
-  )
-}
+class Client extends React.Component {
+  constructor () {
+    super()
+    this.state = { open: false }
+  }
 
-Client.defaultProps = {
-  firstName: 'Ellie',
-  numberOfLetters: 3
+  render () {
+    return (
+      <div>
+        <Row onClick={() => {this.setState({ open: !this.state.open })}}>
+          <ClientItem />
+        </Row>
+        <Row>
+          <Col className='letters' xs={10} xsOffset={1}>
+            <Panel collapsible expanded={this.state.open}>
+              some panel content
+            </Panel>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
 export default Client
