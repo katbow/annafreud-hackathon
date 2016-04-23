06ebb358deb31dafe703bd595c0a252937488437
Katherine Bowler <katherine.bowler@gmail.com>
2016-04-23 16:10:40 +0100

    Add set up for Clinician dashboard and connected to Navbar

---
+++ frontend/src/js/views/ClinicianDashboard/clinician_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClinicianDashboard extends Component {
+  render () {
+    return (
+      <div>
+        <h1>I am on the Clinician Dashboard</h1>
+      </div>
+    )
+  }
+}
