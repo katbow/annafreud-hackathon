a9a8236379621ef7aa01e293100912c5e35ccc13
ronan.mccabe <ronan.mccabe@27partners.com>
2016-04-24 15:47:28 +0100

    working!

--- mb.js
+++
@@ -1,9 +0,0 @@
-const mbKey = process.env.NODE_ENV === 'test' ? process.env.MBIRD_TEST : process.env.MBIRD
-const messageBird = require('messagebird')(mbKey)
-
-messageBird.messages.read('03cd79704571b7e06568f01b93037279', (err, response) => {
-  if (err) {
-    console.log(err)
-  }
-  console.log(response)
-})
